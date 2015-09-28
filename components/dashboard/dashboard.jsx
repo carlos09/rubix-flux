fbe37f932c6db39803d5217bbd2edc88229e06ce
carlos <carlos.esquer927@gmail.com>
2015-09-28 10:45:34 -0700

    updates for actions and stores

--- components/dashboard/dashboard.jsx
+++ components/dashboard/dashboard.jsx
@@ -13,7 +13,7 @@ var Dashboard = React.createClass({
       <Grid id="dash-view">
         <Row className="page-heading">
           <Col sm={6} className="text-left">
-            <h1><i className=""></i><span className="vert-middle">Dashboard</span></h1>
+            <h1><i className="material-icons md-36">dashboard</i><span className="vert-middle">Dashboard</span></h1>
           </Col>
           <Col sm={6} className="text-right">
             <h2 className="h1-class">Balance: $XXXXX</h2>
@@ -23,17 +23,20 @@ var Dashboard = React.createClass({
         <Row>
           <Col sm={4} className="account-stats text-center">
             <div className="buckets">
-              <h2 className=""><i className="material-icons md-48"></i><span className="vert-middle">####</span></h2>
+              <h2 className=""><i className="material-icons md-48">visibility</i><span className="vert-middle">####</span></h2>
+              <p>Unique Content Views*</p>
             </div>
           </Col>
           <Col sm={4} className="account-stats text-center">
             <div className="buckets">
-              <h2 className=""><i className="material-icons md-48"></i><span className="vert-middle">####</span></h2>
+              <h2 className=""><i className="material-icons md-48">local_atm</i><span className="vert-middle">####</span></h2>
+              <p>Generated Revenue*</p>
             </div>
           </Col>
           <Col sm={4} className="account-stats text-center">
             <div className="buckets">
-              <h2 className=""><i className="material-icons md-48"></i><span className="vert-middle">####</span></h2>
+              <h2 className=""><i className="material-icons md-48">wifi_tethering</i><span className="vert-middle">####</span></h2>
+              <p>Managed Stations*</p>
             </div>
           </Col>
         </Row>
@@ -43,8 +46,9 @@ var Dashboard = React.createClass({
             <p className="ast-info">* Metrics updated at 12am PST daily</p>
           </Col>
         </Row>
-
+        <Row>
         <ManageAct />
+        </Row>
       </Grid>
     );
   }
--- components/dashboard/manageAct.jsx
+++ components/dashboard/manageAct.jsx
@@ -1,19 +1,50 @@
-var React = require('react');
-var Login = require('../auth/app-login');
-var AuthStore = require('../../stores/app-auth.js');
-var AuthenticationMixin = require('../../mixins/AuthenticationMixin.js');
-
+import React from 'react';
+import Login from '../auth/app-login';
+import AuthStore from '../../stores/app-auth.js';
+import AuthenticationMixin from '../../mixins/AuthenticationMixin.js';
+//import DashActions from '../../actions/app-actions';
+import DashActions from '../../actions/dash-action';
 
 var manageAct = React.createClass({
   mixins: [ AuthenticationMixin ],
+  getData: function() {
+    console.log('test1');
+    //DashActions.getEntityData();
+  },
+  getInitialState: function(){
+     this.dataChanged = this.dataChanged.bind(this);
+     return {
+       open: false,
+       class: "account-container",
+       stationPrivacy: false
+     }
+  },
+ handleClick: function(){
+   if(this.state.open) {
+     this.setState({
+       open: false,
+       class: "account-container",
+     });
+   }else{
+     this.setState({
+       open: true,
+       class: "account-container open"
+     });
+   }
+ },
+ dataChanged: function(data) {
+   data = { staton: "New text comes here" }
+ },
   render: function () {
     var token = AuthStore.authGetToken();
     return (
-      <Row>
+      <div>
         <Col sm={12}>
           <h2 className="h1-class">Manage Accounts</h2>
+          <div onClick={this.getData()}>Get info:</div> {this.props.getData}
         </Col>
-      </Row>
+
+      </div>
     );
   }
 });
